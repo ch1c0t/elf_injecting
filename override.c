1f7d7649874e4793d98c6f09b8f381bd90842719
Anatoly Chernow <chertoly@gmail.com>
2018-01-31 03:03:46 +0200

    Spawn a new thread from the preloaded

--- override.c
+++ override.c
@@ -2,6 +2,9 @@
 #include <dlfcn.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <pthread.h>
+
+void* perform_work(void*);
 
 typedef int (*LibcStartMainFnType)(int (*main)(int, char **, char **), int argc, char **argv,
                                    int (*init)(void), void (*fini)(void),
@@ -12,11 +15,23 @@ int __libc_start_main(int (*main)(int, char **, char **), int argc, char **argv,
                       void (*ldso_fini)(void), void (*stack_end)) {
   LibcStartMainFnType original = (LibcStartMainFnType)dlsym(RTLD_NEXT, "__libc_start_main");
   puts("before original");
+
+  pthread_t tid;
+  pthread_create(&tid, NULL, perform_work, NULL);
+
   int result = original(main, argc, argv, init, fini, ldso_fini, stack_end);
   // puts("after original"); / doesn't work; why?
   return result;
 }
 
+void*
+perform_work(void* arg) {
+  for(;;) {
+    puts("from preloaded");
+    sleep(2);
+  }
+}
+
 // gcc -Wall -fPIC -DPIC -c override.c
-// ld -shared -o override.so override.o -ldl
+// ld -shared -o override.so override.o -ldl -lpthread
 // LD_PRELOAD=./override.so ruby sleeper.rb
