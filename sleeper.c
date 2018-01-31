52495739d4974acebb5fa4fd9c8ccb14a0cc3527
Anatoly Chernow <chertoly@gmail.com>
2018-01-31 02:38:28 +0200

    Using pthread.h

--- sleeper.c
+++ sleeper.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
+
+void*
+fun(void* vargp) {
+  for(;;) {
+    sleep(1);
+    puts("fun from new thread");
+  }
+}
 
 int
 main() {
+  pthread_t tid;
+
+  pthread_create(&tid, NULL, fun, NULL);
+
   puts("Something");
   sleep(30);
 }
+
+// gcc sleeper.c -lpthread
+// ./a.out
