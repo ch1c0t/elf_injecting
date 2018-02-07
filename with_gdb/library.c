c304d2d64b835aa4007e73ea263134880a85e57b
Anatoly Chernow <chertoly@gmail.com>
2018-02-07 23:33:26 +0200

    Spawn a new thread from library.c constructor

--- with_gdb/library.c
+++ with_gdb/library.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 
 void __attribute__((constructor)) startup();
 void __attribute__((destructor)) shutdown();
+void* perform_work(void* arg);
 
 void
 startup() {
   puts("library loaded");
+
+  pthread_t tid;
+  pthread_create(&tid, NULL, perform_work, NULL);
 }
 
 void
 shutdown() {
   puts("library unloaded");
+}
+
+void*
+perform_work(void* arg) {
+  for(;;) {
+    puts("from preloaded");
+    sleep(2);
+  }
 }
