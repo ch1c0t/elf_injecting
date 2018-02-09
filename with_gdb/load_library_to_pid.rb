761f93c63d7d070caacfec5e7e1e624856c46193
Anatoly Chernow <chertoly@gmail.com>
2018-02-09 23:09:51 +0200

    Expand relative paths to a library

--- with_gdb/load_library_to_pid.rb
+++ with_gdb/load_library_to_pid.rb
@@ -1,7 +1,7 @@
 require 'gdb'
 
 pid = ARGV[0]
-library = ARGV[1] || "#{__dir__}/library.so"
+library = File.expand_path (ARGV[1] || "#{__dir__}/library.so")
 
 gdb = GDB::GDB.new "-p #{pid}"
 gdb.execute "set $dlopen = (void*(*)(char*, int)) dlopen"
