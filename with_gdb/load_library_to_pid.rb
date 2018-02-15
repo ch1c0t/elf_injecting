258b336c0c1a20a4f43448770a75476e95322dc5
Anatoly Chernow <chertoly@gmail.com>
2018-02-15 22:00:25 +0200

    Attach to a process with a separate command

--- with_gdb/load_library_to_pid.rb
+++ with_gdb/load_library_to_pid.rb
@@ -3,6 +3,7 @@ require 'gdb'
 pid = ARGV[0]
 library = File.expand_path (ARGV[1] || "#{__dir__}/library.so")
 
-gdb = GDB::GDB.new "-p #{pid} -q"
+gdb = GDB::GDB.new '-q'
+gdb.execute "attach #{pid}"
 gdb.execute "set $dlopen = (void*(*)(char*, int)) dlopen"
 gdb.execute "call $dlopen(\"#{library}\", 1)"
