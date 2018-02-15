5d631266a39ce071b1e83b19906341f644534dd0
Anatoly Chernow <chertoly@gmail.com>
2018-02-15 21:58:47 +0200

    Omit version information on gdb startup

--- with_gdb/load_library_to_pid.rb
+++ with_gdb/load_library_to_pid.rb
@@ -3,6 +3,6 @@ require 'gdb'
 pid = ARGV[0]
 library = File.expand_path (ARGV[1] || "#{__dir__}/library.so")
 
-gdb = GDB::GDB.new "-p #{pid}"
+gdb = GDB::GDB.new "-p #{pid} -q"
 gdb.execute "set $dlopen = (void*(*)(char*, int)) dlopen"
 gdb.execute "call $dlopen(\"#{library}\", 1)"
